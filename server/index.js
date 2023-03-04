cd6226e69fff2e92ef27b810a359853b6f0fb2f5
Winnard Arthur <winnardarthur7@gmail.com>
2023-03-04 13:35:24 +0000

    prepare for production

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
@@ -14,9 +15,25 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 
 app.use('/posts', postRoutes);
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    let __dirname = path.resolve();
+
+    app.use(express.static(path.join(__dirname + "frontend/build")))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend/build/index.html"))
+    })
+
+} else {
+    app.get("/", (req, res) => {
+        res.send("API running successfully...")
+    })
+}
+
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
