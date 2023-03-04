72f1a8851de506e9d176b32cc5a801b955fc65cd
Winnard Arthur <winnardarthur7@gmail.com>
2023-03-04 15:19:07 +0000

    change production url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000'})
+const API = axios.create({ baseURL: 'https://memories-app-xn71.onrender.com/'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
